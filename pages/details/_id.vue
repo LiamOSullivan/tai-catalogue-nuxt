2b6ad56c5047c10c3691a7c24c400895f9e94b12
Liam <lmosulli@tcd.ie>
2022-11-24 15:09:19 +0000

    add attributes for cd-grass and clara in-situ

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -25,7 +25,9 @@
       <div v-if="date_range" class="card date_range text-left m-1">
         <div class="card-body">
           <h6 class="card-title">Date range</h6>
-          <p class="card-text small">{{ date_range }}</p>
+          <p class="card-text small">
+            <small>{{ date_range }} </small>
+          </p>
         </div>
       </div>
 
@@ -38,8 +40,28 @@
         <div class="card-body">
           <h6 class="card-title">{{ mapName(key) }}</h6>
           <p class="card-text small">
-            {{ value !== null ? capitalise(value) : "Not available" }}
+            <small>{{
+              value !== null ? capitalise(value) : "Not available"
+            }}</small>
           </p>
+        </div>
+      </div>
+      <div class="card text-left m-1 attributes">
+        <div class="card-body">
+          <h6 class="card-title">Attributes</h6>
+          <div v-if="hasAttributes" class="card-text small">
+            <b>Value type | Name | Unit</b>
+            <ul>
+              <li v-for="(attr, key) in attributes" :key="key">
+                <small>
+                  {{ attr["Value type"] }} | {{ attr.Name }} | {{ attr.Unit }}
+                </small>
+              </li>
+            </ul>
+          </div>
+          <div v-else class="card-text small">
+            <small> There are no attributes listed for this dataset </small>
+          </div>
         </div>
       </div>
     </div>
@@ -60,6 +82,10 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import GeoJSON from "ol/format/GeoJSON";
 import nameMap from "../../data/propsNameMap.json";
+import validSites from "~/data/in-situ_valid_sites_attributes.json";
+
+import Papa from "papaparse";
+
 import { capitalise } from "../../utilities";
 library.add(faArrowLeft);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
@@ -79,12 +105,16 @@ export default Vue.extend({
       res_title: string;
       date_range: string;
       error_msg: string;
+      attributes: string[];
+      hasAttributes: boolean;
     } = {
       data: {},
       poly: null,
       res_title: null,
       date_range: null,
       error_msg: null,
+      attributes: [],
+      hasAttributes: false,
     };
 
     return data;
@@ -94,7 +124,8 @@ export default Vue.extend({
     const baseURLJson =
       "https://tai-api.terrainai.com/api/v1/dc/dc-id-data/?format=json";
     const id = this.$route.params.id;
-    // console.log("getting details for row " + id);
+    console.log("getting details for row " + id);
+
     try {
       const res = await axios.get(`${baseURLJson}&dc_id=${id}`);
       const features = new GeoJSON().readFeatures(res.data[0], {
@@ -118,7 +149,7 @@ export default Vue.extend({
         date_to,
         ...displayProps
       } = features[0].getProperties();
-      console.log(displayProps);
+      // console.log(displayProps);
       this.data = displayProps;
       this.poly =
         features[0].getGeometry() !== undefined
@@ -126,6 +157,30 @@ export default Vue.extend({
           : null;
       this.res_title = capitalise(res_title);
       this.date_range = getDateRangeString(date_from, date_to);
+
+      this.hasAttributes = !!validSites[tai_id];
+      if (this.hasAttributes) {
+        const attrURL =
+          "https://taidashboardlayers.blob.core.windows.net/dashboard-storage/catalogue_temp_data_sources/IE_CRA_attribute_table.csv";
+        // let attributes: any = [];
+        const self = this;
+        Papa.parse(attrURL, {
+          download: true,
+          header: true,
+          skipEmptyLines: true,
+          complete: function (results: any) {
+            if (results && results.errors) {
+              if (results.errors.length > 0) {
+                console.log("Results errors: ", results.errors);
+              }
+              if (results.data && results.data.length > 0) {
+                console.log("Results:", results.data);
+                self.attributes = results.data;
+              }
+            }
+          },
+        });
+      }
     } catch (error) {
       console.warn("Error fetching data from Catalogue API", error);
       this.error_msg = `Error fetching data for id of ${this.$route.params.id} - are details correct?`;
@@ -167,8 +222,8 @@ export default Vue.extend({
     "res_abs res_abs res_abs ref bbox-map bbox-map"
     "topic_cat topic_cat sub_cat sub_cat bbox-map bbox-map"
     "res_type res_type res_lang res_lang bbox-map bbox-map"
-    "keyword keyword date_range date_range name name"
-    "instrument lineage lineage lineage spatial_res spatial_res"
+    "keyword keyword date_range date_range name  spatial_res"
+    "instrument lineage lineage attributes attributes attributes"
     "resp_org resp_org poc poc meta_date meta_date"
     "resp_org resp_org poc poc meta_lang meta_lang"
     "conditions conditions conditions limitations limitations limitations"
@@ -255,6 +310,10 @@ export default Vue.extend({
   grid-area: res_lang;
 }
 
+.attributes {
+  grid-area: attributes;
+}
+
 #bbox-map__container {
   display: flex;
   flex-direction: column;
@@ -270,8 +329,30 @@ export default Vue.extend({
   padding: 8px;
 }
 .card-text {
-  max-height: 20vh;
   overflow: auto;
+  max-height: 240px;
+}
+
+h2,
+h6 {
+  font-weight: 400;
+}
+
+p,
+ul,
+li,
+small {
+  font-weight: 200;
+}
+
+b {
+  font-weight: 300;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
 
